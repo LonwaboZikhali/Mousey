e5dc801f8d5963a1243ddcf255598d20ef36b95f
Lonwabo <lozikhajhb024@student.wethinkcode.co.za>
2024-12-13 20:55:36 +0200

    tidying up a bit

--- Mousey.py
+++ Mousey.py
@@ -1,20 +1,19 @@
 import pygame
 import random
 import sys
+
 pygame.init()
 
-score = 0
-
 #background
+
 fullscreen = (1366,710)
 displaysurface = pygame.display.set_mode(fullscreen,pygame.RESIZABLE)
 color = (222,222,0)
-pygame.display.set_caption('NwablesGame')
+pygame.display.set_caption('Mousey')
 
-#what is mousey doing
 # 1. load the image then remove its background
 # 2. resized image
-# 3. goes hand in hand with convert alpha to remove background
+# 3. uses convert alpha to remove background
 
 def fetchimage(imagename,size):
     image = (pygame.image.load(imagename).convert_alpha())
@@ -22,6 +21,8 @@ def fetchimage(imagename,size):
     imageresized.set_colorkey("white")
     
     return imageresized
+
+#what is mousey doing
 
 mouseup = fetchimage('assets/Mouseup.png',0.25)
 mousedown = fetchimage('assets/Mousedown.png',0.25)
@@ -41,17 +42,12 @@ pygame.display.set_icon(Icon)
 
 # white background 
 
-pygame.time.delay(1000)
+pygame.time.delay(700)
 pygame.Surface.fill(displaysurface,"white")
-pygame.display.flip()
-
-# add instruction
-
-pygame.time.delay(900)
 displaysurface.blit(instruction,(0,-55))
 pygame.display.flip()
 
-#back to white background
+# onto game
 
 pygame.time.delay(8000)
 pygame.Surface.fill(displaysurface,"white")
@@ -86,9 +82,15 @@ rsmiddle = (fullscreen[0] - paddingright) + (paddingright/2)
 
 #create points block
 
+score = 0
+
 def createpointsystem():
+
     gap = 10
     
+    scoreboard = str(score)
+    scoreboard2 = str((score)) #score for now but the maximum!!!!!!!!
+
     def scores(text,texttop):
     #set font
         fontsize = 50
@@ -118,18 +120,12 @@ def createpointsystem():
     scores('HIGHSCORE', (rstop + gap + gap + gap + 50 + gap + 210 + gap + gap))
     scoreblocks((gap + rstop + 50 + gap + 210 + gap + 50 + gap + gap + gap + gap),scoreboard2)
 
-
-scoreboard = str(score)
-scoreboard2 = str((score)) #score for now but the maximum!!!!!!!!
-
 def addtoscore():
-
+    
     global score
-    global scoreboard
 
     score += 5
-    scoreboard = str(score)
-    scoreboard2 = str(score)
+    return score
 
 
 pygame.display.flip() 
@@ -148,8 +144,8 @@ cheeseparameters = {
     'vertical':((paddingabove+borderwidth + cheese.get_height() + 5),(paddingabove + borderwidth + arenaheight - cheese.get_height() - 5))
     }
 
-
 #cheese changes position
+
 
 def elusivecheese():
     pygame.time.delay(80)
@@ -160,86 +156,91 @@ def elusivecheese():
 
     pygame.display.flip()
 
-    global a,b
-
     a = random.randrange(cheeseparameters['horizontal'][0],cheeseparameters['horizontal'][1])
     b = random.randrange(cheeseparameters['vertical'][0],cheeseparameters['vertical'][1])
     
+    return a,b
 
+def main():
 
-a,b = (cheeseparameters['horizontal'][1] - cheese.get_width()),(paddingabove + 10)
-x,y = 30,30
+    a,b = (cheeseparameters['horizontal'][1] - cheese.get_width()),(paddingabove + 10)
 
-move_x,move_y = 0,0
-img = mousestop
-speed = 3
+    x,y = 30,30
 
-running = True
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    keys = pygame.key.get_pressed()
     move_x,move_y = 0,0
     img = mousestop
+    speed = 3
 
-    if keys[pygame.K_UP]:
-        img = mouseup
-        move_y = -speed
 
-    elif keys[pygame.K_DOWN]:
-        img = mousedown
-        move_y = +speed
+    running = True
+    while running:
 
-    elif keys[pygame.K_RIGHT]:
-        img = mouseright
-        move_x = +speed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    elif keys[pygame.K_LEFT]:
-        img = mouseleft
-        move_x = -speed
-
-    else:
+        keys = pygame.key.get_pressed()
+        move_x,move_y = 0,0
         img = mousestop
-        move_x = 0
 
-    if keys[pygame.K_ESCAPE]:
-        running = False    
+        if keys[pygame.K_UP]:
+            img = mouseup
+            move_y = -speed
 
-    x += move_x
-    y += move_y
+        elif keys[pygame.K_DOWN]:
+            img = mousedown
+            move_y = +speed
 
-# there is conflict between the parameters and where
-#you are saying you want to be, and the parameters win
+        elif keys[pygame.K_RIGHT]:
+            img = mouseright
+            move_x = +speed
 
-    x = max(parameters['horizontal'][0], min(x,parameters['horizontal'][1] - img.get_width()))
-    y = max(parameters['vertical'][0], min(y,parameters['vertical'][1] - img.get_height()))
+        elif keys[pygame.K_LEFT]:
+            img = mouseleft
+            move_x = -speed
+
+        else:
+            img = mousestop
+            move_x = 0
+
+        if keys[pygame.K_ESCAPE]:
+            running = False    
+
+        x += move_x
+        y += move_y
+
+    # there is conflict between the parameters and where
+    #you are saying you want to be, and the parameters win (keeping within the parameters)
+
+        x = max(parameters['horizontal'][0], min(x,parameters['horizontal'][1] - img.get_width()))
+        y = max(parameters['vertical'][0], min(y,parameters['vertical'][1] - img.get_height()))
 
 
-    displaysurface.fill(pygame.Color('#ADD8E6'))
-    arena = createarena()
-    pointsblock = createpointsystem()
+        displaysurface.fill(pygame.Color('#ADD8E6'))
+        arena = createarena()
+        pointsblock = createpointsystem()
 
-    mymouse = displaysurface.blit(img,(x,y))
-    
-    mycheese = displaysurface.blit(cheese,(a,b))
-    pygame.display.flip()
-
-       #Mouse eats cheese
-
-    mouserect = pygame.Rect(x,y,img.get_width(),img.get_height())
-    cheeserect = pygame.Rect(a,b,cheese.get_width(),cheese.get_height())
-
-    def collision():
+        mymouse = displaysurface.blit(img,(x,y))
         
-        return mouserect.colliderect(cheeserect)
+        mycheese = displaysurface.blit(cheese,(a,b))
+        pygame.display.flip()
 
-    if collision():
-        elusivecheese()
-        addtoscore()
-        
-        #also add a high score remembering function
-        #maybe add some music?
-pygame.quit()
+        #Mouse eats cheese
+
+        mouserect = pygame.Rect(x,y,img.get_width(),img.get_height())
+        cheeserect = pygame.Rect(a,b,cheese.get_width(),cheese.get_height())
+
+        def collision():
+            
+            return mouserect.colliderect(cheeserect)
+
+        if collision():
+            elusivecheese()
+            addtoscore()
+            
+            #also add a high score remembering function
+            #maybe add some music?
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
